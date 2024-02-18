44d6200e4c1803df5f88cf64aaf8876081275131
andrew <>
2024-02-18 21:14:34 +0800

    added Comment on code

--- main.c
+++ main.c
@@ -1,9 +1,3 @@
-/****
- * Basic Guide to reading this code,
- * 
- * 
- * 
-*/
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/fs.h>
@@ -12,21 +6,18 @@
 #include <linux/kdev_t.h>
 #include <linux/gpio.h>
 #include <linux/delay.h>
-#include <linux/mutex.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrew_Infinite");
 MODULE_DESCRIPTION("Bit_Bang Module");
-#define DRIVER_NAME "BitBangDevice"
-#define DRIVER_CLASS "BitBangClass"
-#define TX_PIN 17
-#define RX_PIN 27
+#define DEVICE_NAME "BitBangDevice"
+#define DEVICE_CLASS "BitBangClass"
 
-/*Prototype*/
-static int device_file_open(struct inode *, struct file *);
-static int device_file_close(struct inode *, struct file *);
-static ssize_t device_file_read(struct file *, char *, size_t, loff_t *);
-static ssize_t device_file_write(struct file *, const char *, size_t, loff_t *);
+/* Module param */
+static int TX_PIN = 17;
+static int RX_PIN = 27;
+module_param(TX_PIN, int, 0644);
+module_param(RX_PIN, int, 0644);
 
 /* Buffer for data */
 #define buffer_cap 2000
@@ -36,9 +27,16 @@ static char buffer_receive[buffer_cap];
 
 /* Variables for device and device class */
 static dev_t my_device_num;
-static struct class *my_class;
+static struct class *my_device_class;
 static struct cdev my_device;
 
+/* Prototype */
+static int device_file_open(struct inode *, struct file *);
+static int device_file_close(struct inode *, struct file *);
+static ssize_t device_file_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_file_write(struct file *, const char *, size_t, loff_t *);
+
+/* FileSystem operation registration */
 static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = device_file_open,
@@ -46,6 +44,8 @@ static struct file_operations fops = {
 	.read = device_file_read,
 	.write = device_file_write
 };
+
+
 
 /**
  * @brief Filesys open, load file to memory
@@ -72,18 +72,18 @@ static int device_file_close(struct inode *device_file, struct file *instance)
  */
 static ssize_t device_file_read(struct file *File, char *user_buffer, size_t count, loff_t *offset) 
 {
-	int to_copy, not_copied, delta;
+	int size_can_copy, size_failed, size_succeed;
 
-	/* Get amount of data to copy */
-	to_copy = min(count, buffer_end_ptr);
+	/* Get amount of data can copy */
+	size_can_copy = min(count, buffer_end_ptr);
 
 	/* Copy data to user */
-	not_copied = copy_to_user(user_buffer, buffer_receive, to_copy);
+	size_failed = copy_to_user(user_buffer, buffer_receive, size_can_copy);
 
-	/* Calculate data */
-	delta = to_copy - not_copied;
+	/* Calculate number of data successfully copied */
+	size_succeed = size_can_copy - size_failed;
 
-	return delta;
+	return size_succeed;
 }
 
 /**
@@ -91,52 +91,66 @@ static ssize_t device_file_read(struct file *File, char *user_buffer, size_t cou
  */
 static ssize_t device_file_write(struct file *File, const char *user_buffer, size_t count, loff_t *offset) 
 {
-	int to_copy, not_copied, delta;
+	int size_can_copy, size_failed, size_succeed;
 
-	/* Get amount of data to copy */
-	to_copy = min(count, sizeof(buffer_send));
+	/* Get amount of data can copy */
+	size_can_copy = min(count, sizeof(buffer_send));
 
-	/* Copy data to user */
-	not_copied = copy_from_user(buffer_send, user_buffer, to_copy);
+	/* Copy data to kernel */
+	size_failed = copy_from_user(buffer_send, user_buffer, size_can_copy);
 
-	/* Calculate data */
-	delta = to_copy - not_copied;
+	/* Calculate number of data successfully copied */
+	size_succeed = size_can_copy - size_failed;
+
+	/* screen clear */
 	if(strncmp(buffer_send,"clear\n",6) == 0)
 	{
 		char *clear = "\033[2J\033[H";
+		/* With each Byte of $clear, send data */
 		while(*clear)
 		{
 			int j = -1;
+			/* Start bit send */
 			gpio_set_value(TX_PIN, 0);
-			udelay(1000000 / 9600);
+			udelay(1000000 / 9600); // Baud rate: 9600
+			/* With each bit of the byte from $clear */
 			while(++j < 8)
 			{
+				/* Send Bit */
 				gpio_set_value(TX_PIN, (*clear >> j) & 0x01);
-				udelay(1000000 / 9600);  // Baud rate: 9600
+				udelay(1000000 / 9600); 
 			}
+			/* End Bit */
 			gpio_set_value(TX_PIN, 1);
 			udelay(1000000 / 9600);
+			/* Move to next byte */
 			clear++;
 		}
 	}
 	else
 	{
 		int i = -1;
-		while(++i < delta)
+		/* With each Byte of $buffer_send, send data */
+		while(++i < size_succeed)
 		{
 			int j = -1;
+			/* Start bit send */
 			gpio_set_value(TX_PIN, 0);
-			udelay(1000000 / 9600);
+			udelay(1000000 / 9600); // Baud rate: 9600
+			/* With each bit of the byte from $buffer_send */
 			while(++j < 8)
 			{
+				/* Send Bit */
 				gpio_set_value(TX_PIN, (*(buffer_send + i) >> j) & 0x01);
 				udelay(1000000 / 9600);  // Baud rate: 9600
 			}
+			/* End Bit */
 			gpio_set_value(TX_PIN, 1);
 			udelay(1000000 / 9600);
 		}
 	}
-	return delta;
+
+	return size_succeed;
 }
 
 /**
@@ -144,23 +158,21 @@ static ssize_t device_file_write(struct file *File, const char *user_buffer, siz
 */
 static int __init ModuleInit(void)
 {
-	int retval;
-
 	/* Dynamically allocate a device number */
-	if( alloc_chrdev_region(&my_device_num, 0, 1, DRIVER_NAME) < 0) {
+	if( alloc_chrdev_region(&my_device_num, 0, 1, DEVICE_NAME) < 0) {
 		printk("ERROR: Bit_Bang Module: Device Number could not be allocated!\n");
 		return -1;
 	}
 	printk("Bit_Bang Module: number: Major: %d, Minor: %d!\n", MAJOR(my_device_num), MINOR(my_device_num));
 
 	/* Create device class */
-	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+	if((my_device_class = class_create(THIS_MODULE, DEVICE_CLASS)) == NULL) {
 		printk("ERROR: Bit_Bang Module: Device class can not be created!\n");
 		goto ClassError;
 	}
 
-	/* create device file */
-	if(device_create(my_class, NULL, my_device_num, NULL, DRIVER_NAME) == NULL) {
+	/* Create device file */
+	if(device_create(my_device_class, NULL, my_device_num, NULL, DEVICE_NAME) == NULL) {
 		printk("ERROR: Bit_Bang Module: Can not create device file!\n");
 		goto FileError;
 	}
@@ -168,17 +180,18 @@ static int __init ModuleInit(void)
 	/* Initialize device file */
 	cdev_init(&my_device, &fops);
 
-	/* Regisering device to kernel */
+	/* Registering device to kernel */
 	if(cdev_add(&my_device, my_device_num, 1) == -1) {
 		printk(KERN_ERR "ERROR: Bit_Bang Module: Registering of device to kernel failed!\n");
 		goto AddError;
 	}
 
+	/* Registering GPIO pin */
     if (gpio_request(TX_PIN, "tx_pin") || gpio_request(RX_PIN, "rx_pin")) {
         printk(KERN_ERR "ERROR: Bit_Bang Module: Failed to request GPIO pins\n");
         goto GpioReqError;
     }
-
+	/* Set GPIO Mode */
     if(gpio_direction_output(TX_PIN, 1) || gpio_direction_input(RX_PIN))
     {
         printk(KERN_ERR "ERROR: Bit_Bang Module: Failed to config pins\n");
@@ -186,15 +199,16 @@ static int __init ModuleInit(void)
     }
 	return 0;
 
+/* Destruct allocated things if error */
 GpioSetError:
     gpio_free(TX_PIN);
     gpio_free(RX_PIN);
 GpioReqError:
     /*do nothing*/;
 AddError:
-    device_destroy(my_class, my_device_num);
+    device_destroy(my_device_class, my_device_num);
 FileError:
-    class_destroy(my_class);
+    class_destroy(my_device_class);
 ClassError:
     unregister_chrdev_region(my_device_num, 1);
     return -1;
@@ -210,8 +224,8 @@ static void __exit ModuleExit(void)
     gpio_free(RX_PIN);
 
     cdev_del(&my_device);
-	device_destroy(my_class, my_device_num);
-	class_destroy(my_class);
+	device_destroy(my_device_class, my_device_num);
+	class_destroy(my_device_class);
 	unregister_chrdev_region(my_device_num, 1);
     printk("Bit_Bang Module unloaded\n");
 }
